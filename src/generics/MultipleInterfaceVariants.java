c58e9197e58141fcc7692e800d332a7530a01745
xiaominglei001 <xiaominglei001@qq.com>
2017-09-28 11:08:13 +0800

    Initial commit

---
+++ src/generics/MultipleInterfaceVariants.java
@@ -0,0 +1,11 @@
+//: generics/MultipleInterfaceVariants.java
+package generics; /* Added by Eclipse.py */
+// {CompileTimeError} (Won't compile)
+
+interface Payable<T> {}
+
+class Employee implements Payable<Employee> {}
+/*
+class Hourly extends Employee
+  implements Payable<Hourly> {} ///:~
+  */
